171e860a8f3804b5e5b0caeb60a3bf6c31cb6f70
unknown <nhutchodiennguyen23062004@gmail.com>
2024-03-12 10:00:20 +0700

    bài 1

---
+++ src/app/authen/AuthenStackNavigation.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Login from './Login/Login'
+// import SplashScreen from './SplashScreen/SplashScreen'
+const Stack = createNativeStackNavigator()
+
+const AuthenStackNavigation = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }} >   
+            <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+    )
+}
+
+export default AuthenStackNavigation
