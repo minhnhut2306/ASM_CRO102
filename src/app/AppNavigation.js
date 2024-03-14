d5b706f4b798b7e6f4b1711495121ee39fc58aae
unknown <nhutchodiennguyen23062004@gmail.com>
2024-03-14 10:45:02 +0700

    bottom

--- src/app/AppNavigation.js
+++ src/app/AppNavigation.js
@@ -3,14 +3,14 @@ import React, { useContext } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import AuthenStackNavigation from './authen/AuthenStackNavigation'
 import { AppContext } from './main/AppContext'
+import MainTabNavigation from './main/MainStackNavigation'
 
 const AppNavigation = () => {
     const { isLogin } = useContext(AppContext);
     return (
         <NavigationContainer>
             {
-                
-                    <AuthenStackNavigation /> 
+                           <MainTabNavigation /> 
             }
         </NavigationContainer>
     )
---
+++ src/app/main/MainStackNavigation.js
@@ -0,0 +1,58 @@
+import {View, Text, Image, StyleSheet} from 'react-native';
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from './tabs/Home/Home';
+import Account from './tabs/Account/Account';
+import Notification from './tabs/Notification/Notification';
+import Search from './tabs/Search/Search';
+
+const tabIcons = {
+  Home: require('../../../assets/images/home.png'),
+  Search: require('../../../assets/images/search.png'),
+  Favorite: require('../../../assets/images/bell.png'),
+  Notification: require('../../../assets/images/user.png'),
+};
+
+const MainTabNavigation = () => {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused}) => {
+          const iconSource = tabIcons[route.name];
+          return (
+            <Image
+              source={iconSource}
+              style={[styles.icon, focused && styles.focusedIcon]}
+            />
+          );
+        },
+        tabBarLabel: ({focused}) => {
+          return null; 
+        },
+        tabBarStyle: {
+          backgroundColor: 'white',
+        },
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Favorite" component={Account} />
+      <Tab.Screen name="Notification" component={Notification} />
+    </Tab.Navigator>
+  );
+};
+
+export default MainTabNavigation;
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+  focusedIcon: {
+    tintColor: '#D17842',
+  },
+});
